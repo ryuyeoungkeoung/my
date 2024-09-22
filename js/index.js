daac502c0addb3544b9271ac31d5aec0ec3028de
ryuyeoungkeoung <yok1812@naver.com>
2024-09-23 00:53:41 +0900

    수정본

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
 "use strict";
 //커서 태그들
+const blobCursor = (() => {
+  const cursor = document.querySelector("#cursorBlob");
+  const links = document.querySelectorAll(".nav__link");
+  const setCursorPos = (e) => {
+    const { pageX: posX, pageY: posY } = e;
+    cursor.style.top = `${posY - cursor.offsetHeight / 2}px`;
+    cursor.style.left = `${posX - cursor.offsetWidth / 2}px`;
+  };
+  document.addEventListener("mousemove", setCursorPos);
+
+  const setCursorHover = () => (cursor.style.transform = "scale(2.5)");
+  const removeCursorHover = () => (cursor.style.transform = "");
+  links.forEach((link) => link.addEventListener("mouseover", setCursorHover));
+  links.forEach((link) =>
+    link.addEventListener("mouseleave", removeCursorHover)
+  );
+})();
 
 // 프로플 사진
 gsap.registerPlugin(ScrollTrigger);
@@ -53,6 +70,34 @@ ScrollTrigger.matchMedia({
         // markers: true,
       },
       y: "200%",
+    });
+  },
+});
+
+// 스킬트리
+// 스크롤 트리거
+ScrollTrigger.matchMedia({
+  // 1000px 이상일 때
+  "(min-width: 1000px)": function () {
+    gsap.from(".Pigma", {
+      scrollTrigger: {
+        duration: 100,
+        trigger: "body",
+        start: "0% 0%",
+        end: "0% 0%",
+        scrub: 1,
+
+        // markers: true,
+      },
+      y: "0%",
+    });
+    gsap.to(".Pigma", {
+      scrollTrigger: {
+        trigger: "four",
+        display: block,
+        // markers: true,
+      },
+      y: "0%",
     });
   },
 });
